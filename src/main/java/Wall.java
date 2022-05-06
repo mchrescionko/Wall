2240e8cf95c74526474edea3d68100d5b140e65e
Maciej Chrescionko <m.chrescionko@gmail.com>
2022-05-06 13:25:18 +0200

    change solution

--- src/main/java/Wall.java
+++ src/main/java/Wall.java
@@ -1,52 +1,63 @@
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.Stack;
 import java.util.stream.Collectors;
 
 interface Structure {
     // zwraca dowolny element o podanym kolorze
     Optional findBlockByColor(String color);
 
+
     // zwraca wszystkie elementy z danego materiału
     List findBlocksByMaterial(String material);
+
 
     //zwraca liczbę wszystkich elementów tworzących strukturę
     int count();
 }
 
 public class Wall implements Structure {
-    private List<CompositeBlock> blocks;
+    private List<Block> blocks;
 
-    @Override
-    public Optional<CompositeBlock> findBlockByColor(String color) {
-        return blocks.stream()
-                .filter(b -> b.getColor().equals(color))
-                .findAny();
+    public List<Block> unwindBlocks() {
+        List<Block> singleBlock = new ArrayList<>();
+        Stack<Block> toCheck = new Stack<>();
+        toCheck.addAll(blocks);
+        while (!toCheck.empty()) {
+            Block check = toCheck.pop();
+            singleBlock.add(check);
+            try {
+                CompositeBlock composite = (CompositeBlock) check;
+                toCheck.addAll(composite.getBlocks());
+            } catch (Exception ex) {
+            }
+        }
+        return singleBlock;
     }
 
     @Override
-    public List<CompositeBlock> findBlocksByMaterial(String material) {
-        return blocks.stream()
-                .filter(b -> b.getMaterial().equals(material))
-                .collect(Collectors.toList());
+    public Optional findBlockByColor(String color) {
+        return unwindBlocks().stream().filter(e -> e.getColor().equals(color)).findAny();
+    }
+
+    @Override
+    public List findBlocksByMaterial(String material) {
+        return unwindBlocks().stream().filter(e -> e.getMaterial().equals(material)).collect(Collectors.toList());
     }
 
     @Override
     public int count() {
-        int result = 0;
-        for (CompositeBlock block : blocks) {
-            result += block.getBlocks().size();
-        }
-        return result;
+        return unwindBlocks().size();
     }
-}
 
-interface Block {
-    String getColor();
+    interface Block {
+        String getColor();
 
-    String getMaterial();
-}
+        String getMaterial();
+    }
 
-
-interface CompositeBlock extends Block {
-    List getBlocks();
+    interface CompositeBlock extends Block {
+        List getBlocks();
+    }
 }
